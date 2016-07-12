147ef3d1ef842e314b260eb8c29a13a43e7019f0
Seiji Shii <seiji.shii@gmail.com>
2016-07-11 22:48:50 -0500

    MapActivityのレイアウトをドローワにした。 ドローワメニュー部分をスクロールビューにした。 ドロワーを開閉できるようになった。

--- app/src/main/java/net/c_kogyo/returnvisitor/MapActivity.java
+++ app/src/main/java/net/c_kogyo/returnvisitor/MapActivity.java
@@ -1,13 +1,51 @@
 package net.c_kogyo.returnvisitor;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MapActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        createToolBar();
+        createDrawer();
+
+
+
+    }
+
+    // ドロワーを開閉するにはToolBarが必要。
+    // AppThemeはNoActionBarに指定しないとWindow Decorと衝突する
+    private Toolbar toolbar;
+    private void createToolBar(){
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+//        toolbar.inflateMenu(R.menu.return_visitor_menu);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
+    }
+
+    private DrawerLayout navDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private void createDrawer() {
+
+        navDrawer = (DrawerLayout) findViewById(R.id.drawer);
+        // toolBarを設定するコンストラクタを使用する必要がある
+        mDrawerToggle = new ActionBarDrawerToggle(this, navDrawer, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
     }
 }
