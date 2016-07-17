84dc708d9079578159e446c46ba606eb44b7db8e
Seiji Shii <seiji.shii@gmail.com>
2016-07-16 21:22:24 -0500

    Google Sign inの先のFirebaseAuthをコピペで実装（まだ途中）

--- app/src/main/java/net/c_kogyo/returnvisitor/LoginSelectDialog.java
+++ app/src/main/java/net/c_kogyo/returnvisitor/LoginSelectDialog.java
@@ -6,10 +6,12 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.MapView;
 
 /**
  * Created by SeijiShii on 2016/07/16.
@@ -18,27 +20,37 @@ import android.view.ViewGroup;
 public class LoginSelectDialog extends DialogFragment {
 
     private static Context mContext;
+    private static MapActivity.OnGoogleSignInClickListener mOnGoogleSignInListener;
+    private static MapActivity.OnFBLoginClickListener mOnFBLoginClickListener;
+    private static MapActivity.EmailLoginClickListener mEmailLoginClickListener;
 
-    public static LoginSelectDialog newInstance(Context context) {
+    private View view;
+
+
+    public static LoginSelectDialog newInstance(Context context,
+                                                MapActivity.OnGoogleSignInClickListener onGoogleSignInListener,
+                                                MapActivity.OnFBLoginClickListener onFBLoginClickListener,
+                                                MapActivity.EmailLoginClickListener emailLoginClickListener) {
 
         mContext = context;
+        mOnGoogleSignInListener = onGoogleSignInListener;
+        mOnFBLoginClickListener = onFBLoginClickListener;
+        mEmailLoginClickListener = emailLoginClickListener;
 
         return new LoginSelectDialog();
     }
-
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//
-//        return inflater.inflate(R.layout.login_select_dialog, null);
-//
-//    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-        View view = LayoutInflater.from(mContext).inflate(R.layout.login_select_dialog, null);
+        view = LayoutInflater.from(mContext).inflate(R.layout.login_select_dialog, null);
+
+        createEmailText();
+        createPasswordText();
+        createLoginButton();
+        createGoogleLoginButton();
+        createFBLoginButton();
 
         builder.setTitle(R.string.select_login);
         builder.setView(view);
@@ -51,6 +63,59 @@ public class LoginSelectDialog extends DialogFragment {
         });
 
         return builder.create();
+    }
+
+    private EditText emailText;
+    private void createEmailText() {
+
+        emailText = (EditText) view.findViewById(R.id.email_text);
+
+    }
+
+    private EditText passwordText;
+    private void createPasswordText() {
+        passwordText = (EditText) view.findViewById(R.id.password_text);
+    }
+
+    private void createLoginButton() {
+
+        Button loginButton = (Button) view.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mEmailLoginClickListener.onClick(emailText.getText().toString(), passwordText.getText().toString());
+                dismiss();
+
+            }
+        });
+    }
+
+    private void createGoogleLoginButton() {
+
+        Button gLoginButton = (Button) view.findViewById(R.id.google_login_button);
+        gLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mOnGoogleSignInListener.onClick();
+                dismiss();
+            }
+        });
+    }
+
+    private void createFBLoginButton() {
+
+        Button fbLoginButton = (Button) view.findViewById(R.id.fb_login_button);
+        fbLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mOnFBLoginClickListener.onClick();
+                dismiss();
+
+            }
+        });
     }
 }
 
--- app/src/main/java/net/c_kogyo/returnvisitor/MapActivity.java
+++ app/src/main/java/net/c_kogyo/returnvisitor/MapActivity.java
@@ -1,30 +1,47 @@
 package net.c_kogyo.returnvisitor;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.facebook.appevents.AppEventsLogger;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.facebook.FacebookSdk;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapActivity extends AppCompatActivity
+                            implements OnMapReadyCallback,
+                                        GoogleApiClient.OnConnectionFailedListener {
+
 
     static final String MAP_DEBUG ="map_debug";
 
@@ -37,9 +54,13 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     MapView mMapView;
     GoogleMap mMap;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initFirebaseAuth();
+        initGoogleSignIn();
 
         setContentView(R.layout.activity_map);
 
@@ -66,6 +87,22 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onPause();
 
         saveCameraPosition();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+
     }
 
     private void loadCameraPosition() {
@@ -150,6 +187,41 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     }
 
+    private GoogleSignInOptions gso;
+    private GoogleApiClient mGoogleApiClient;
+    private void initGoogleSignIn() {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private void initFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+    }
+
+
     private void facebookLogin() {
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
@@ -158,14 +230,99 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private void createLoginButton() {
 
         Button loginButton = (Button) findViewById(R.id.login_button);
+        
+        if (mAuth.)
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                LoginSelectDialog.newInstance(MapActivity.this).show(getFragmentManager(), "Login_dialog");
+                LoginSelectDialog.newInstance(MapActivity.this,
+                                                new OnGoogleSignInClickListener(),
+                                                new OnFBLoginClickListener(),
+                                                new EmailLoginClickListener())
+                                                .show(getFragmentManager(), "Login_dialog");
             }
         });
 
+    }
+
+    class EmailLoginClickListener {
+
+        void onClick(String email, String password) {
+            Toast.makeText(MapActivity.this, "Email Sign in Clicked", Toast.LENGTH_SHORT).show();
+            navDrawer.closeDrawer(Gravity.LEFT);
+        }
+    }
+
+    private static final Integer RC_SIGN_IN = 716;
+
+    class OnGoogleSignInClickListener {
+
+        void onClick() {
+//            Toast.makeText(MapActivity.this, "Google Sign in Clicked", Toast.LENGTH_SHORT).show();
+            navDrawer.closeDrawer(Gravity.LEFT);
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private static final String GOOGLE_SIGN_IN_TAG = "google_sign_in_tag";
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(GOOGLE_SIGN_IN_TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Toast.makeText(this, acct.getDisplayName(), Toast.LENGTH_SHORT).show();
+
+            firebaseAuthWithGoogle(acct);
+//            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+//            updateUI(false);
+        }
+    }
+
+    private static final String FIREBASE_TAG = "firebase_tag";
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(FIREBASE_TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(FIREBASE_TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(FIREBASE_TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(MapActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    class OnFBLoginClickListener {
+
+        void onClick() {
+            Toast.makeText(MapActivity.this, "FB Log in Clicked", Toast.LENGTH_SHORT).show();
+            navDrawer.closeDrawer(Gravity.LEFT);
+        }
     }
 
 }
