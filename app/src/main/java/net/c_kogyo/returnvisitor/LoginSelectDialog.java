fe6de88c9705bf8e30b6e012f7962471334a1e55
Seiji Shii <seiji.shii@gmail.com>
2016-07-16 20:16:28 -0500

    .gitignoreを書き換える

--- app/src/main/java/net/c_kogyo/returnvisitor/LoginSelectDialog.java
+++ app/src/main/java/net/c_kogyo/returnvisitor/LoginSelectDialog.java
@@ -1,10 +1,56 @@
 package net.c_kogyo.returnvisitor;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by SeijiShii on 2016/07/16.
  */
 
 public class LoginSelectDialog extends DialogFragment {
+
+    private static Context mContext;
+
+    public static LoginSelectDialog newInstance(Context context) {
+
+        mContext = context;
+
+        return new LoginSelectDialog();
+    }
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//
+//        return inflater.inflate(R.layout.login_select_dialog, null);
+//
+//    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.login_select_dialog, null);
+
+        builder.setTitle(R.string.select_login);
+        builder.setView(view);
+        builder.setNegativeButton(R.string.cancel_text, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dismiss();
+            }
+        });
+
+        return builder.create();
+    }
 }
+
--- app/src/main/java/net/c_kogyo/returnvisitor/MapActivity.java
+++ app/src/main/java/net/c_kogyo/returnvisitor/MapActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.facebook.appevents.AppEventsLogger;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -134,31 +135,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                                         .position(mMap.getCameraPosition().target)
                                         .icon(BitmapDescriptorFactory.fromResource(R.mipmap.marker_blue)));
 
-//
-//        LatLng latLng = new LatLng(20.694882,-101.369367);
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-//        Log.d(MAP_DEBUG, latLng.latitude + ", " + latLng.longitude);
-
-
-//        LatLng latLng = mMap.getCameraPosition().target;
-//        Log.d(MAP_DEBUG, latLng.latitude + ", " + latLng.longitude);
-//
-//        mMap.moveCamera(CameraUpdateFactory.zoomTo(13f));
-//        Log.d(MAP_DEBUG, "Zoom Level = " + mMap.getCameraPosition().zoom);
-//
-//
-//        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
-//            @Override
-//            public void onCameraChange(CameraPosition cameraPosition) {
-//                Log.d(MAP_DEBUG, "Camera Position Changed!");
-//                Log.d(MAP_DEBUG, "Zoom Level = " + mMap.getCameraPosition().zoom);
-//                LatLng latLng = cameraPosition.target;
-//                Log.d(MAP_DEBUG, latLng.latitude + ", " + latLng.longitude);
-
-//            }
-//        });
-
-
     }
 
     private DrawerLayout navDrawer;
@@ -170,11 +146,26 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mDrawerToggle = new ActionBarDrawerToggle(this, navDrawer, toolbar, R.string.app_name, R.string.app_name);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
 
+        createLoginButton();
+
     }
 
     private void facebookLogin() {
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+    }
+
+    private void createLoginButton() {
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LoginSelectDialog.newInstance(MapActivity.this).show(getFragmentManager(), "Login_dialog");
+            }
+        });
+
     }
 
 }
