7080fe6a090d52cdad74000452f7eb4b604ab91e
SeijiShii <seiji.shii@gmail.com>
2016-07-19 14:51:31 -0500

    追加　TimePeriodDataItem, Visit

---
+++ app/src/main/java/net/c_kogyo/returnvisitor/data/TimePeriodDataItem.java
@@ -0,0 +1,49 @@
+package net.c_kogyo.returnvisitor.data;
+
+import android.content.Context;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+/**
+ * Created by 56255 on 2016/07/19.
+ */
+public class TimePeriodDataItem extends BaseDataItem{
+
+    public static final String VISIT_ID = "visit_id";
+
+    public static final String START = "start";
+    public static final String END = "end";
+
+    private Calendar start;
+    private Calendar end;
+
+    public TimePeriodDataItem() {
+
+        super();
+
+        this.start  = Calendar.getInstance();
+        this.end    = Calendar.getInstance();
+    }
+
+    @Override
+    public HashMap<String, Object> toMap() {
+
+        HashMap<String, Object> map = super.toMap();
+
+        map.put(START,  start.getTimeInMillis());
+        map.put(END,    end.getTimeInMillis());
+
+        return map;
+    }
+
+    @Override
+    public String getIdHeader() {
+        return null;
+    }
+
+    @Override
+    public String toStringForSearch(Context context) {
+        return null;
+    }
+}
--- app/src/main/java/net/c_kogyo/returnvisitor/data/Visit.java
+++ app/src/main/java/net/c_kogyo/returnvisitor/data/Visit.java
@@ -1,8 +1,48 @@
 package net.c_kogyo.returnvisitor.data;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
 /**
  * Created by 56255 on 2016/07/19.
  */
-public class Visit {
+public class Visit extends TimePeriodDataItem{
 
+    public static final String VISIT_ID = "visit_id";
+    public static final String PLACE_ID = "place_id";
+    public static final String PERSON_IDS = "person_ids";
+
+    private String placeId;
+    private ArrayList<String> personIds;
+
+    public Visit() {
+        super();
+
+        this.placeId = null;
+        this.personIds = new ArrayList<>();
+    }
+
+    @Override
+    public String getIdHeader() {
+        return VISIT_ID;
+    }
+
+    @Override
+    public String toStringForSearch(Context context) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> toMap() {
+
+        HashMap<String, Object> map = super.toMap();
+
+        map.put(PLACE_ID, placeId);
+        map.put(PERSON_IDS, personIds);
+
+        return map;
+    }
 }
