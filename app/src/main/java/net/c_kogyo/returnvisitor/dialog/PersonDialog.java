bf99682f92ac9d043c4c2d820952d528bac3bd16
Seiji Shii <seiji.shii@gmail.com>
2016-07-23 21:05:43 -0500

    Added PersonDialog

--- app/src/main/java/net/c_kogyo/returnvisitor/dialog/PersonDialog.java
+++ app/src/main/java/net/c_kogyo/returnvisitor/dialog/PersonDialog.java
@@ -1,8 +1,15 @@
 package net.c_kogyo.returnvisitor.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
+import net.c_kogyo.returnvisitor.R;
 import net.c_kogyo.returnvisitor.data.Person;
 
 /**
@@ -12,15 +19,34 @@ import net.c_kogyo.returnvisitor.data.Person;
 public class PersonDialog extends DialogFragment {
 
     private static Person mPerson;
+    private Context mContext;
 
     public static PersonDialog getInstance(Person person) {
 
         mPerson = person;
 
-
         return new PersonDialog();
 
     }
 
+    private View v;
 
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        v = LayoutInflater.from(getActivity()).inflate(R.layout.person_dialog, null);
+        builder.setView(v);
+
+        builder.setPositiveButton(R.string.ok_text, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setNegativeButton(R.string.cancel_text, null);
+
+        return builder.create();
+    }
 }
