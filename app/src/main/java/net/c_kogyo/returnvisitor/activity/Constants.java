996406b7de24e3cda10dbc17bef0e5f1b1f3e0ba
Seiji Shii <seiji.shii@gmail.com>
2016-07-24 18:32:26 -0500

    Add PersonCell

---
+++ app/src/main/java/net/c_kogyo/returnvisitor/activity/Constants.java
@@ -0,0 +1,20 @@
+package net.c_kogyo.returnvisitor.activity;
+
+import net.c_kogyo.returnvisitor.R;
+
+/**
+ * Created by SeijiShii on 2016/07/24.
+ */
+
+public class Constants {
+
+    public static final int[] buttonRes = {
+            R.mipmap.button_gray,
+            R.mipmap.button_red,
+            R.mipmap.button_purple,
+            R.mipmap.button_blue,
+            R.mipmap.button_green,
+            R.mipmap.button_yellow,
+            R.mipmap.button_orange
+    } ;
+}
---
+++ app/src/main/java/net/c_kogyo/returnvisitor/view/PersonCell.java
@@ -0,0 +1,8 @@
+package net.c_kogyo.returnvisitor.view;
+
+/**
+ * Created by SeijiShii on 2016/07/24.
+ */
+
+public class PersonCell extends BaseAnimateView {
+}
