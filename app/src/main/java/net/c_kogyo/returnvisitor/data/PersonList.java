da780be74407c81deca93d3b5efb3ccaf91128f0
Seiji Shii <seiji.shii@gmail.com>
2016-07-28 08:07:00 -0500

    Created PersonList, VisitList

---
+++ app/src/main/java/net/c_kogyo/returnvisitor/data/PersonList.java
@@ -0,0 +1,10 @@
+package net.c_kogyo.returnvisitor.data;
+
+import DataList;
+
+/**
+ * Created by SeijiShii on 2016/07/28.
+ */
+
+public class PersonList extends DataList {
+}
---
+++ app/src/main/java/net/c_kogyo/returnvisitor/data/VisitList.java
@@ -0,0 +1,8 @@
+package net.c_kogyo.returnvisitor.data;
+
+/**
+ * Created by SeijiShii on 2016/07/28.
+ */
+
+public class VisitList extends DataList {
+}
