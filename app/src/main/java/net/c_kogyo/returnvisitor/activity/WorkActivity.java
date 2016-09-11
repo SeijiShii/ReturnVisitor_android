2336c1635aae3197dccfaf36cab20fd7725f2763
Seiji Shii <seiji.shii@gmail.com>
2016-09-11 10:43:20 -0500

    VisitCell, WorkView added

---
+++ app/src/main/java/net/c_kogyo/returnvisitor/activity/WorkActivity.java
@@ -0,0 +1,10 @@
+package net.c_kogyo.returnvisitor.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SeijiShii on 2016/08/21.
+ */
+
+public class WorkActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/net/c_kogyo/returnvisitor/util/CalendarUtil.java
@@ -0,0 +1,8 @@
+package net.c_kogyo.returnvisitor.util;
+
+/**
+ * Created by SeijiShii on 2016/08/24.
+ */
+
+public class CalendarUtil {
+}
